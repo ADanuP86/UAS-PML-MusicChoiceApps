969a7d215fdabb93d48d0dd685f0f52c77253428
ADanuP86 <anantodanuprasetyo02@gmail.com>
2021-10-26 21:07:44 +0700

    commit uts pml

---
+++ android/app/src/main/kotlin/com/example/projectflutter_musiccoiceapps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projectflutter_musiccoiceapps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:projectflutter_musiccoiceapps/screens/library.dart';
+//import 'package:projectflutter_musiccoiceapps/screens/nowplaying.dart';
+
+void main() {
+  runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Library(),
+    )
+  );
+}
